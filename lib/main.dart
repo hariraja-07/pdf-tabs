c43ccc3a0530b3e8a6311ad7d6068c4d5bbc5f20
hariraja-07 <hariraja1976@gmail.com>
2026-08-06 22:53:17 +0530

    feat: open PDFs from other apps via Android intent

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pdfrx/pdfrx.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
 import 'core/theme/app_theme.dart';
 import 'features/home/screens/home_screen.dart';
+import 'features/pdf_viewer/screens/pdf_viewer_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,16 +15,62 @@ void main() {
   runApp(const ProviderScope(child: PdfTabsApp()));
 }
 
-class PdfTabsApp extends ConsumerWidget {
+class PdfTabsApp extends ConsumerStatefulWidget {
   const PdfTabsApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<PdfTabsApp> createState() => _PdfTabsAppState();
+}
+
+class _PdfTabsAppState extends ConsumerState<PdfTabsApp> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+  StreamSubscription<List<SharedMediaFile>>? _mediaSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => _handleInitialMedia());
+    _mediaSubscription =
+        ReceiveSharingIntent.instance.getMediaStream().listen(_openSharedFiles);
+  }
+
+  @override
+  void dispose() {
+    _mediaSubscription?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _handleInitialMedia() async {
+    final files = await ReceiveSharingIntent.instance.getInitialMedia();
+    if (files.isNotEmpty) _openSharedFiles(files);
+  }
+
+  void _openSharedFiles(List<SharedMediaFile> files) {
+    final navigator = _navigatorKey.currentState;
+    if (navigator == null) return;
+
+    for (final file in files) {
+      final path = file.path;
+      if (path.isEmpty || !path.toLowerCase().endsWith('.pdf')) continue;
+
+      final name = path.split('/').last;
+      navigator.push(
+        MaterialPageRoute(
+          builder: (_) => PdfViewerScreen(filePath: path, fileName: name),
+        ),
+      );
+      return;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final themeMode = ref.watch(themeModeProvider);
 
     return MaterialApp(
       title: 'PdfTabs',
       debugShowCheckedModeBanner: false,
+      navigatorKey: _navigatorKey,
       theme: AppTheme.lightTheme(),
       darkTheme: AppTheme.darkTheme(),
       themeMode: themeMode,
